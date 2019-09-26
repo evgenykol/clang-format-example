f7c38555055f8bf33acdefa3fcfe854999eabb96
evgenykol <edkolotilov@gmail.com>
2019-09-26 14:28:27 +0300

    ClangFormat.cmake add target _check

--- cmake/ClangFormat.cmake
+++ cmake/ClangFormat.cmake
@@ -3,6 +3,17 @@
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
+# https://github.com/zemasoft/clangformat-cmake
+
+function(add_target_wrap target_name)
+  if(TARGET ${target_name})
+    add_dependencies(${target_name} ${PROJECT_NAME}_${target_name})
+  else()
+    add_custom_target(${target_name} DEPENDS ${PROJECT_NAME}_${target_name})
+  endif()
+endfunction()
+
+
 function(clangformat_setup)
   if(NOT CLANGFORMAT_EXECUTABLE)
     set(CLANGFORMAT_EXECUTABLE clang-format)
@@ -33,11 +44,26 @@ function(clangformat_setup)
       "Formating with ${CLANGFORMAT_EXECUTABLE} ..."
   )
 
-  if(TARGET clangformat)
-    add_dependencies(clangformat ${PROJECT_NAME}_clangformat)
-  else()
-    add_custom_target(clangformat DEPENDS ${PROJECT_NAME}_clangformat)
-  endif()
+  add_custom_target(${PROJECT_NAME}_clangformat_check
+    COMMAND
+      ${CLANGFORMAT_EXECUTABLE}
+      -style=file
+      -output-replacements-xml
+      ${clangformat_sources}
+      #print output
+      | tee ${CMAKE_BINARY_DIR}/check_format_file.txt | grep -c "replacement " |
+                tr -d "[:cntrl:]" && echo " replacements necessary"
+
+    # stop with error if there are problems
+    COMMAND ! grep -c "replacement "
+                ${CMAKE_BINARY_DIR}/check_format_file.txt > /dev/null
+    COMMENT
+      "Checking format compliance"
+  )
+
+  add_target_wrap(clangformat)
+  add_target_wrap(clangformat_check)
+
 endfunction()
 
 function(target_clangformat_setup target)
